dc5bb5db7c3b9f5c7ae66131d5375f8a1d2101ea
jasonemarkson <jasonmarkson76@gmail.com>
2021-03-20 11:23:41 -0400

    commit tree 6c465ffdfaa132ed799ba6979b89e9278436248d

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+// map
+
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    for (const element of sourceArray) {
+        newArray.push(element * -1)
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = []
+    for (const element of sourceArray) {
+        newArray.push(element * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = []
+    for (const element of sourceArray) {
+        newArray.push(element ** 2)
+    }
+    return newArray
+}
+
+
+// reduce
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    // let total = sourceArray.forEach(element => startingPoint + element)
+    // return total
+    let accum = startingPoint
+    sourceArray.forEach(element => accum = accum + element)
+    return accum
+}
+
+function reduceToAllTrue(sourceArray) {
+    if (sourceArray.every(element => !!element === true)) {
+        return true
+    }
+    else {
+        return false
+    }
+    // if (sourceArray.forEach(element => element === false)) {
+    //     return false
+    // } else {
+    //     return true
+    // }
+}
+
+function reduceToAnyTrue(sourceArray) {
+    if (sourceArray.find(element => element === true)) {
+        return true
+    } else {
+        return false
+    }
+}
